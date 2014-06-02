3911797d20de73aee236bc5e3a18a67d51e760aa
NikiMaslarski <jjniki93@gmail.com>
2014-06-02 17:20:37 +0300

    added Player

--- Player/Player.py
+++ Player/Player.py
@@ -1,3 +1,18 @@
+import os
+import sys
+os.chdir('../Towns')
+sys.path.append(os.getcwd)
+
 class Player:
     def __init__(self, race):
-        pass
+        self.town = Town(race)
+        self.level = 1
+        self.experience = 0
+        self.experience_to_level_up = 200*self.level
+
+
+    def can_level_up(self):
+        if self.experience_to_level_up < self.experience:
+            self.level += 1
+            self.experience -= self.experience_to_level_up
+
